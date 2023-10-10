eaf402f23da88a66dfb127e37fd8d3a8efd3dd37
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-10-10 18:40:04 +0100

    setup Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Button } from '@radix-ui/themes'
 
 const IssuesPage = () => {
-  return <div>Issues</div>
+  return (
+    <div>
+      Issues
+      <Button>New Theme</Button>
+    </div>
+  )
 }
 
 export default IssuesPage

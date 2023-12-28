483718a6352b07367525fef1075509ff8442ab96
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-12-28 10:46:02 +0100

    sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -25,10 +25,15 @@ const IssuesPage = async ({ searchParams }: Props) => {
     ? searchParams.status
     : undefined
 
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: 'asc' }
+    : undefined
+
   const issues = await prisma.issue.findMany({
-    where: {
-      status,
-    },
+    where: { status },
+    orderBy,
   })
 
   return (
